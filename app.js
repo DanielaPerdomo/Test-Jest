34320d3559bd8e93c0a3c51a3551f304fb25613a
DanielaPerdomo <98133250+DanielaPerdomo@users.noreply.github.com>
2023-06-20 12:50:55 +0000

    Modificacion al archivo app.js

--- app.js
+++ app.js
@@ -28,20 +28,3 @@ console.log(sum(7,3))
 
 // tenemos que incluir la funcion en el exports para que sea exportada a otros archivos como test.js
 module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound}
-
-/* codigo de funcion
-let oneEuroIs = {
-    "JPY": 127.9, // japan yen
-    "USD": 1.2, // us dollar
-    "GBP": 0.8, // british pound
-}
-let fromDollarToYen = function(oneEuroIs) {
-    return oneEuroIs * "JPY";
-};
-let fromEuroToDollar = function(oneEuroIs) {
-    return oneEuroIs * "USD";
-};
-let fromYenToPound = function(oneEuroIs) {
-    return oneEuroIs * "USD";
-}; */
-
